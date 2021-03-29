7af770169246c35b4d289d914a980f93967ace09
jeffpborges <jeffpborges@gmail.com>
2021-03-29 11:17:59 -0300

    changing app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1> Hi there . Starting out my friends. </h1>
+				<h1> Hi there. yoy Starting out my friends. </h1>
 			</div>
 		)
 	}
