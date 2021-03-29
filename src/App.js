d6870ae92d8e47272e65d9f01f03cb4aaf07de19
jeffpborges <jeffpborges@gmail.com>
2021-03-29 11:12:13 -0300

    adding new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h1> Hi there . Starting out my friends. </h1>
+			</div>
+		)
+	}
+}
+
+export default App;
